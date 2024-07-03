786bbc618ff80945b956d8208d969b21eeaea206
dukcapil balikpapan <ahmadmuhrani89@gmail.com>
2024-07-03 08:33:16 +0800

    update juli

--- app/Models/slide.php
+++ app/Models/slide.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class slide extends Model
 {
     use HasFactory;
+    public function getImage()
+    {
+        if (substr($this->image, 0, 5) == "https"){
+            return $this->image;
+        }
+
+        if ($this->image){
+            return asset('storage/app/public/slide/image/'. $this->image);
+        }
+    }
 }
